9196abe9c5f01b4e37e760e36fef1dfe14a2b19e
indreshp135 <indreshp135@gmail.com>
2022-01-29 22:15:09 +0530

    feat: routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import "./App.css";
 import Web3ContextProvider from "./context/Web3Context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Landing } from "./components/Landing";
 
 function App() {
   return (
     <Web3ContextProvider>
-      <div className="App">
-        <h1 className="text-5xl font-extrabold">Enigma</h1>
-        <button
-          className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          type="button"
-        >
-          Enigma
-        </button>
-      </div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Landing/>} />
+        </Routes>
+      </Router>
     </Web3ContextProvider>
   );
 }
