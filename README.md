db484d9c6622cef39341dd9defcdc6e5422e4718
siva2204 <sivaraman26r@gmail.com>
2022-01-29 15:07:10 +0530

    [initial] setup

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # trinitt-hack
 Tri nitt hack
+
+## Install truffle
+```bash
+    npm i -g truffle
+```
+
+## local blockchain setup
+- install [ganache](https://trufflesuite.com/ganache) or use `truffle develop` command
+
+## truffle commands (run in `truffle console` or use truffle as prefix)
+- `compile`
+- `migrate`
+- `test`
+
+## client setup
+```bash
+    npm install && npm start
+```
---
+++ truffle-config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    develop: {
+      host: "127.0.0.1", // ganache host
+      port: 7545, //ganache port
+      network_id: "*",
+    },
+  },
+};
